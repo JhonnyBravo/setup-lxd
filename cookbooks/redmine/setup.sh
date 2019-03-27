be3661b62ee41b3421481de15ca4834afa41f7ac
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-03-27 21:21:10 +0900

    modified: cookbooks/redmine/setup.sh
    new option: n
      ホストマシンから lxd コンテナへのポート転送設定オプションを追加。

--- cookbooks/redmine/setup.sh
+++ cookbooks/redmine/setup.sh
@@ -4,8 +4,13 @@ script_name=$(basename "$0")
 
 package="apache2 libapache2-mod-passenger mysql-server mysql-client redmine redmine-mysql"
 
+myhost=''
+trusthost=''
+container=''
+
 i_flag=0
 u_flag=0
+n_flag=0
 
 function usage(){
 cat <<_EOT_
@@ -13,7 +18,7 @@ NAME
   ${script_name}
 
 USAGE
-  ${script_name} [-i] [-u] [-h]
+  ${script_name} [-i] [-u] [-n] [-h]
 
 DESCRIPTION
   Redmine をインストール / アンインストールします。
@@ -21,12 +26,13 @@ DESCRIPTION
 OPTIONS
   -i  Redmine をインストールします。
   -u  Redmine をアンインストールします。
+  -n  ホストマシンから lxd コンテナへのポート転送を設定します。
   -h  ヘルプを表示します。
 _EOT_
 exit 1
 }
 
-while getopts "iuh" option
+while getopts "iunh" option
 do
   case $option in
     i)
@@ -34,6 +40,9 @@ do
       ;;
     u)
       u_flag=1
+      ;;
+    n)
+      n_flag=1
       ;;
     h)
       usage
@@ -48,4 +57,7 @@ if [ $i_flag -eq 1 ]; then
   apt-get install ${package}
 elif [ $u_flag -eq 1 ]; then
   apt-get purge ${package}
+elif [ $n_flag -eq 1 ]; then
+  iptables -A INPUT -p tcp --syn -m state --state NEW -s $trusthost -d $myhost --dport 8000 -j ACCEPT
+  iptables -t nat -A PREROUTING -d $myhost -p tcp -m tcp --dport 8000 -j DNAT --to-destination ${container}:8000
 fi
